8b8610a5249fe8ab6a2d9fd4fbcc77cdcba5f28f
Anton <yushkevich.a.a@yandex.ru>
2024-02-12 20:55:02 +0300

    change config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import 
-  tsconfigPaths
- from 'vite-tsconfig-paths';
+// import 
+//   tsconfigPaths
+//  from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wheel",
-  plugins: [react(), tsconfigPaths()],
+  base: "/guess-the-wiki/",
+  plugins: [react()],
 })
